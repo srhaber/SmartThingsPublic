f760efab1cadc4fed0b13ff0ad44a30c68b4ecf2
Shaun Haber <srhaber@gmail.com>
2015-10-13 22:28:21 -0700

    first section

--- smartapps/srhaber/rf-outlets.src/rf-outlets.groovy
+++ smartapps/srhaber/rf-outlets.src/rf-outlets.groovy
@@ -5,11 +5,14 @@ definition(
 	description: "Remotely control RF outlet switches from SmartThings.",
 	category: "My Apps",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
 )
 
 preferences {
-	section("RF Outlets Setup")
+	section("RF Outlets Setup") {
+		// Put something here
+	}
 	//page(name:"sonosDiscovery", title:"Sonos Device Setup", content:"sonosDiscovery", refreshTimeout:5)
 }
 
